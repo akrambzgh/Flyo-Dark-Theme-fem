02401e46e550521a8b80c2d49ed1340a8f363119
Akram Bouzoualegh <89752239+akrambzgh@users.noreply.github.com>
2022-01-18 16:20:57 +0100

    Add the last section and the email checker is done

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,14 @@
-console.log("hello");
+const emailInput = document.querySelector(".email");
+const submitButton = document.querySelector(".get")
+let errorText = document.querySelector(".error")
+
+submitButton.addEventListener("click", () => {;
+    let value = emailInput.value
+    let com = value.substr(-4)
+
+    if(com != ".com") {
+        errorText.classList.add("there-is")()
+    } else {
+        errorText.classList.remove("there-is")
+    }
+})
